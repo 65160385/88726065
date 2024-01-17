999d66bdbe3f4f97a2592a593908bfc0e087196c
65160385 <65160385@go.buu.ac.th>
2024-01-17 14:59:34 +0700

    toggle รายการ todo

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -24,10 +24,10 @@ document.addEventListener("DOMContentLoaded", function () {
     renderTodoList();
     }
     // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
-    // function toggleComplete(index) {
-    // todos[index].completed = !todos[index].completed;
-    // renderTodoList();
-    // }
+    function toggleComplete(index) {
+    todos[index].completed = !todos[index].completed;
+    renderTodoList();
+    } 
     // แสดงรายการ Todo บนหน้าเว็บ
     function renderTodoList() {
     console.log(todos);
